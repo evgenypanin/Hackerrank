75a7c056aeeca2c08c6de4cad8a319e3ea00cb11
evgenypanin <91653524+evgenypanin@users.noreply.github.com>
2021-09-30 12:28:41 +0300

    Create Class.cpp

---
+++ Class.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+/*
+Enter code for class Student here.
+Read statement for specification.
+*/
+
+class Student
+{
+private:
+    int age;
+    int standard;
+    string first_name;
+    string last_name;
+public:
+    void set_age(int & n) {age = n;}
+    void set_standard(int & n) {standard = n;}
+    void set_first_name(string & b) {first_name = b;}
+    void set_last_name(string & b) {last_name = b;}
+    int get_age() {return age;}
+    int get_standard() {return standard;}
+    string get_first_name() {return first_name;}
+    string get_last_name() {return last_name;}
+    string to_string();
+};
+
+string Student::to_string()
+{
+    string temp;
+    temp = std::to_string(age) + "," + first_name + "," + last_name + "," + std::to_string(standard);
+    return temp; 
+}
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+}
