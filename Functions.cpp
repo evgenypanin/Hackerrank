c4f08cba2e147ae6640e27125cfea32f7b2989b0
evgenypanin <91653524+evgenypanin@users.noreply.github.com>
2021-09-30 09:22:48 +0300

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d);
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+int max_of_four(int a, int b, int c, int d)
+{
+    vector<int> arr = {a, b, c , d};
+    int max = *max_element(arr.begin(), arr.end());
+    return max;
+    
+}
