bbb0e95ceb67c1c0392a42395f1f5a9b0738b588
evgenypanin <91653524+evgenypanin@users.noreply.github.com>
2021-09-30 17:53:44 +0300

    Create Sets_STL.cpp

---
+++ Sets_STL.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int count;
+    cin >> count;
+    set<int> ss;
+    for (int i = 0; i < count; i++)
+    {
+        int type, query;
+        cin >> type >> query;
+        switch (type)
+        {
+            case 1:
+                ss.insert(query);
+                break;
+            case 2:
+                ss.erase(query);
+                break;
+            case 3:
+                cout << (ss.find(query) == ss.end() ? "No" : "Yes") << endl;
+                break;
+        }
+    }   
+    return 0;
+}
+
+
+
